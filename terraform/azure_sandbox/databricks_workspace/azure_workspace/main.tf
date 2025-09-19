dd40e49c137d0635a1d66eb25f246fcfdf72c798
jlieow <52845474+jlieow@users.noreply.github.com>
2025-09-19 16:44:01 +0100

    Update main.tf

--- terraform/azure_sandbox/databricks_workspace/azure_workspace/main.tf
+++ terraform/azure_sandbox/databricks_workspace/azure_workspace/main.tf
@@ -29,6 +29,12 @@ locals {
 	# prefix = "${local.identifier}-azure-workspace"
 }
 
+resource "azurerm_resource_group" "databricks_managed_rg" {
+  name     = "${local.prefix}-databricks-managed-rg"
+  location = "West Europe"
+}
+
+
 resource "azurerm_network_security_group" "databricks_nsg" {
   name                = "${local.prefix}-databricks-nsg"
   location            = data.azurerm_resource_group.fe_shared_emea_001.location
@@ -56,24 +62,25 @@ resource "azurerm_subnet" "private_subnet" {
   address_prefixes     = ["10.0.1.128/25"]
 }
 
-resource "azurerm_databricks_workspace" "workspace" {
-  name                = "${local.prefix}-workspace"
-  resource_group_name = data.azurerm_resource_group.fe_shared_emea_001.name
-  location            = data.azurerm_resource_group.fe_shared_emea_001.location
-  sku                 = "trial"
+# resource "azurerm_databricks_workspace" "workspace" {
+#   name                        = "${local.prefix}-workspace"
+#   resource_group_name         = data.azurerm_resource_group.fe_shared_emea_001.name
+#   managed_resource_group_name = azurerm_resource_group.databricks_managed_rg.name
+#   location                    = data.azurerm_resource_group.fe_shared_emea_001.location
+#   sku                         = "trial"
 	
-	custom_parameters {
-		virtual_network_id                                  = azurerm_virtual_network.databricks_vnet.id
+# 	custom_parameters {
+# 		virtual_network_id                                  = azurerm_virtual_network.databricks_vnet.id
 
-    public_subnet_name                                  = azurerm_subnet.public_subnet.name
-    public_subnet_network_security_group_association_id = azurerm_network_security_group.databricks_nsg.id
+#     public_subnet_name                                  = azurerm_subnet.public_subnet.name
+#     public_subnet_network_security_group_association_id = azurerm_network_security_group.databricks_nsg.id
 
-    private_subnet_name                                  = azurerm_subnet.private_subnet.name
-    private_subnet_network_security_group_association_id = azurerm_network_security_group.databricks_nsg.id
+#     private_subnet_name                                  = azurerm_subnet.private_subnet.name
+#     private_subnet_network_security_group_association_id = azurerm_network_security_group.databricks_nsg.id
 
-    no_public_ip                                         = true # Required as there is an active policy "DatabricksAzureDBClustersDisablePublicIP" which demands that all Databricks clusters/workspaces are created without a public IP and use only private networking.
-	}
-}
+#     no_public_ip                                         = true # Required as there is an active policy "DatabricksAzureDBClustersDisablePublicIP" which demands that all Databricks clusters/workspaces are created without a public IP and use only private networking.
+# 	}
+# }
 
 # resource "azurerm_storage_account" "databricks_storage_account" {
 #   name                     = "${local.prefix}databrickssa"
